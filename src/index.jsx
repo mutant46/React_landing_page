bc64357b69d4361dee72d809538b37a53a6ab5de
junaid <lucifermjm@gmail.com>
2022-02-17 13:09:03 +0500

    hamburger added

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./production/style.css";
+import Btn from "./Components/atoms/hamburger";
 const App = () => {
   return (
     <React.Fragment>
-      <h1>Hello World</h1>
+      <Testing />
     </React.Fragment>
+  );
+};
+
+const Testing = () => {
+  return (
+    <div className="h-screen bg-white w-full flex justify-center items-center">
+      <Btn />
+    </div>
   );
 };
 
